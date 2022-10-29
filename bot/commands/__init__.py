d79e4f4ea8df6cfd4576f24f9cdc1a43c2ee1555
Kuribohrn <87871320+Kuribohrn@users.noreply.github.com>
2022-10-29 11:36:18 -0400

    :fire: refactor(help): Remove help command since it's 2022.
    Remove help command since it's 2022 and we are using slash commands.
    
    BREAKING CHANGE: No more help commands.
    
    Signed-off-by: Kuribohrn <87871320+Kuribohrn@users.noreply.github.com>

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -2,4 +2,4 @@ from bot.commands.misc import misc
 from bot.commands.ranks import activity
 from bot.commands.admin import links
 from bot.commands.ctf import ctfs, solve, verify
-from bot.commands.general import bookmark, helps, poll, welcome
+from bot.commands.general import bookmark, poll, welcome
